e8bed1baa54c080f73a4b955ecd67041590c1c4f
Adrian Bähler <adi@ancilla.ch>
2017-11-06 10:02:09 +0100

    changes

---
+++ client/gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var watchify = require("watchify");
+var tsify = require("tsify");
+var gutil = require("gulp-util");
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+var paths = {
+    pages: ['src/*.html']
+};
+
+var watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/index.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify));
+
+gulp.task("copy-html", function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("copy-assets",function(){
+    return gulp.src("./src/css/playcards/faces/**/*")
+    .pipe(gulp.dest("dist/faces"));
+});
+
+function bundle() {
+    return watchedBrowserify
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest("dist"));
+}
+
+gulp.task('styles', function() {
+    gulp.src('src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task("default", ["copy-html","copy-assets","styles"], bundle);
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutil.log);
